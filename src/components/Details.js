cdd3fd94ee1116d7e174d9327d9ff52327e96bee
Paul <paul@universitylaundry.com>
2021-03-01 14:11:37 -0600

    Updated CRA files

--- src/components/Details.js
+++ src/components/Details.js
@@ -27,13 +27,13 @@ const Details = (props) => {
       <span onClick={() => props.onClick()} style={closeStyle}>X</span>
       <h2 className="repo-title">{props.title}</h2>
       <h4 className="repo-desc">{props.description}</h4>
-      <a href={props.github} target="_blank">View on GitHub</a>
+      <a href={props.github} target="_blank" rel="noreferrer">View on GitHub</a>
       <span className="repo-language">
         Languages: <code style={langStyle}>{!props.language ? 'No Languages Detected' : props.language}</code>
       </span>
       <p style={{ marginBottom: 0 }}>Forks: {props.forks}</p>
       <p>Open Issues: {props.issues}</p>
-      {props.homepage ? <a href={props.homepage} target="_blank">Repo Homepage</a> : null}
+      {props.homepage ? <a href={props.homepage} target="_blank" rel="noreferrer">Repo Homepage</a> : null}
       <span className="stargazers">
         <img src={star} style={starStyle} alt="A Star Icon" />
         {props.starCount}
